483e35cfab394943831a9a54871cd4801b6188c3
vasuonweb <vasuonweb@gmail.com>
2025-07-03 20:13:00 +0530

    commented

--- src/main/java/com/sree/controller/CourseController.java
+++ src/main/java/com/sree/controller/CourseController.java
@@ -57,10 +57,12 @@ public class CourseController {
         }
     }
 
+/*
     @GetMapping("/health")
     public String healthCheck() {
         return "UP";
     }
+*/
 
 
     @GetMapping("/welcome")
